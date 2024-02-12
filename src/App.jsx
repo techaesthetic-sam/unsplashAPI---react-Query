68a4ba79ebf04c21914a8c1882e5da642256adb7
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-02-12 14:49:18 +0530

    toggle theme implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Gallery from "./Gallery";
 import SearchForm from "./SearchForm";
 import ThemeToggle from "./ThemeToggle";
+import { useGlobalContext } from "./context";
 
 const App = () => {
+  // const { isDarkTheme } = useGlobalContext();
+  // console.log(isDarkTheme);
   return (
     <>
       <ThemeToggle />
--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { useGlobalContext } from "./context";
-
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+import "./index.css";
 export default function ThemeToggle() {
-  const { greeting } = useGlobalContext();
-  return <div>{greeting}</div>;
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  //console.log(isDarkTheme);
+  //   const toggle = isDarkTheme ? "dark-theme" : "";
+  //   console.log(toggle);
+  return (
+    
+    <section className="toggle-container">
+      {/* // <section className={`toggle-container ${isDarkTheme ? "dark-theme" : ""}`}> */}
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? <BsFillMoonFill /> : <BsFillSunFill />}
+      </button>
+    </section>
+    
+  );
 }
